c5e9c2302f5b83128c70e5048823eaa823cb2c19
Tapri Andi <tapri.andi@aitindo.com>
2020-01-09 16:34:38 +0700

    day 2

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("header").style.top = "0";
+//   } else {
+//     document.getElementById("header").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+AOS.init();
